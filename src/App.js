6f9a6bc9579df8551f24beba88c2a633385283de
Durrell Jules <dujules23@gmail.com>
2021-01-05 22:47:47 -0500

    add correct classes from app.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
+// imported useState and useEffect
 import React , { useState, useEffect } from "react";
 import "./App.css";
+//imported axios and Posts(component needed to be connected to the main page, app)
 import axios from "axios"
 import Posts from "./Posts"
 
 
 // console.log(useState)
-function App( props ) {
+function App() {
   const [post, setPost] = useState({});
 
-
+  //useEffect
   useEffect(() => {
-    //axios call
+    //axios call, pulls out data from the object, don't forget the dependency array!
     axios.get(`https://api.nasa.gov/planetary/apod?api_key=DCJZcCTd2c7tifAhZjBG0PhseZR6cu0GWj27GgFi`)
   .then(res => {
     setPost(res.data)
@@ -24,6 +26,7 @@ function App( props ) {
   },[]) 
 
   console.log(post)
+  //return with rending of the Post Component
   return (
     <div className="App">
       
--- src/Posts.js
+++ src/Posts.js
@@ -1,32 +1,29 @@
 //imports
-import React , { useState } from "react";
-
-
+import React from "react";
 
 
 
 export default function Posts (props) {
+    //setting up the props which will be used to connect this component to the App.js
     const { post } = props
 
     
-     
+     //layout of the component, returns designated data using jsx
     return (
         <div className="container">
-            <header>Photo of the Day </header>
-            <div className="img">
-                <img src={post.hdurl}></img>
+            <img className="App-logo" alt="logo"></img>
+            <div>
+                <header className="App-header">Photo of the Day </header>
+            </div>    
+            <div className="img" alt="photo-of-the-day">
+                <img src={post.url} alt=""></img>
             </div>
             <div className="img-info">
                 <h3> Copyright: {post.copyright}</h3>
                 <p>Description: {post.explanation}</p>
-                <p>Date: {post.date} </p>
+                    <p>Date: {post.date} </p>
             </div>
         </div>
     )
   
-
-
-    
-    
-
 }  
